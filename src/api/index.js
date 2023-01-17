36a87b9197680c0beeb62aeb60a6af3055e17a70
seodoha <doha.seo89@gmail.com>
2023-01-17 14:42:27 +0900

    개발 환경 세팅

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const config = {
+    // baseUrl: "https://api.hnpwa.com/v0/",
+};
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { router } from "@/routes/index";
+import { store } from "@/store/index";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
+}).$mount("#app");
