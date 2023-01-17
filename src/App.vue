e726ef1e2af62e902be449df1e6f71bee85c0cd5
박서연 <seoyeonpub@gmail.com>
2023-01-17 17:18:57 +0900

    메인화면 작업중

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
 <template>
-    <v-app>
+    <v-app id="inspire">
         <v-main>
             <v-container>
-                <h1>테스트</h1>
+                <v-row>
+                    <!-- TODO: v-for문으로 커피 컴포넌트 -->
+                <v-col
+                    v-for="n in 24"
+                    :key="n"
+                    cols="4"
+                >
+                    <v-card height="200"></v-card>
+                </v-col>
+                </v-row>
             </v-container>
         </v-main>
+        <v-footer padless>
+            <MainFooterBtn/>
+        </v-footer>
     </v-app>
+    
 </template>
 
 <script>
+import MainFooterBtn from "@/components/MainFooterBtn.vue";
 
 export default {
-    name: "App",
+    data() {
+        return{
 
-    components: {
+        }
     },
-
-    data: () => ({
-        //
-    }),
-};
+    components:{
+        MainFooterBtn
+    }
+}
 </script>
 
 <style lang="scss">
---
+++ src/components/MainFooterBtn.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="main-footer">
+        <div>장바구니</div>
+        <button class="default-btn">결제하기</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
