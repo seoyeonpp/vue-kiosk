cac05c15a7c647ad1dae5970ea76e16c0d655bf5
박서연 <seoyeonpub@gmail.com>
2023-01-19 16:58:23 +0900

    장바구니 삭제기능추가

--- src/components/BasketFooter.vue
+++ src/components/BasketFooter.vue
@@ -3,11 +3,16 @@
         <div class="basket-footer">
             <div v-if="!isAdmin">
                 <div class="basket">
-                    <p v-for="(menu, index) in menus" :key="index">
+                    <h4>장바구니</h4>
+                    <p v-for="(item, index) in basketItem" :key="index">
                         <span>
-                            {{ menu }}
-                            <button @click="deleteMenu" class="delete-btn">삭제</button>
+                            {{ item.menuName }}
+                            <button @click="deleteMenu(item.menuSeq)" class="delete-btn">삭제</button>
                         </span>
+                    </p>
+                    <hr />
+                    <p>
+                        <strong class="sum">총 {{ sumCost }} 원</strong>
                     </p>
                 </div>
                 <button class="blue-btn">결제하기</button>
@@ -21,22 +26,32 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapMutations } from "vuex";
 export default {
     data() {
         return {
-            menus: ["아메리카노", "자몽에이드", "카페라떼", "바닐라떼"],
+            sumCost: 0,
         };
     },
     computed: {
         ...mapGetters({
+            drinkList: "getDrinkList",
             isAdmin: "getIsAdmin",
+            basketItem: "getBasketItem",
         }),
     },
     methods: {
-        deleteMenu() {
-            console.log("메뉴삭제");
+        deleteMenu(menuSeq) {
+            this.$store.commit("DELETE_BASKET_ITEM", menuSeq);
+            this.funcSumCost(this.basketItem, "-");
         },
+        funcSumCost(basketItem, operator) {
+            console.log(operator);
+            operator === "+" ? basketItem.map((item) => (this.sumCost += item.menuCost)) : basketItem.map((item) => (this.sumCost -= item.menuCost));
+        },
+    },
+    created() {
+        this.funcSumCost(this.basketItem, "+");
     },
 };
 </script>
